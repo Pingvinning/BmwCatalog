ef8d1f11bcaadda638264fcc16d99720e30e8d10
Pingvinning <dovgopoliyroman261206@gmail.com>
2025-08-04 14:54:50 +0200

    Initial commit

---
+++ src/main/java/com/bmwcatalog/BmwCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.bmwcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmwCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmwcatalog/BmwCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmwcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
